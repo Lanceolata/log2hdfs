a94b07e6e5d0ff70f72c117185e692f848543421
lanceolata <lanceolatayuan@gmail.com>
2017-05-05 11:14:58 +0800

    add script

--- build_log2hdfs.sh
+++ build_log2hdfs.sh
@@ -3,32 +3,11 @@
 g++ -g -W -Wall -std=c++11 \
 -I /data/users/data-infra/log2hdfs/src \
 -I /data/users/data-infra/log2hdfs/thirdparty/installed/include \
--I /usr/hdp/2.4.0.0-169/usr/include/ \
 -L /data/users/data-infra/log2hdfs/thirdparty/installed/lib \
--L /usr/java/jdk1.8.0_77/jre/lib/amd64/server \
--L /usr/hdp/2.4.0.0-169/usr/lib \
--o kafka2hdfs src/kafka2hdfs.cc src/kafka2hdfs/*.cc \
-src/util/*.cc src/kafka/*.cc \
--l pthread -l hdfs -l jvm -l hdfs -l rdkafka
-
-concat_jar_paths()
-{
-    str=$1
-    arr=(${str//:/ })
-    paths=
-    for s in ${arr[*]}
-    do
-        paths="$paths:$s"
-    done
-    echo $paths
-}
-
-classpaths=$(hadoop classpath)
-classpaths=$(concat_jar_paths $classpaths)
+-o log2kafka src/log2kafka/*.cc src/util/*.cc src/kafka/*.cc \
+thirdparty/installed/include/easylogging++.cc \
+-l pthread -l rdkafka -DELPP_THREAD_SAFE -DELPP_NO_DEFAULT_LOG_FILE
 
 export LD_LIBRARY_PATH="/data/users/data-infra/log2hdfs/thirdparty/installed/lib:/usr/java/jdk1.8.0_77/jre/lib/amd64/server:/usr/hdp/2.4.0.0-169/usr/lib:/usr/hdp/2.4.0.0-169/hadoop/lib/native:$LD_LIBRARY_PATH"
-export CLASSPATH=$classpaths
-#export HADOOP_OPTS="$HADOOP_OPTS -Djava.library.path=/usr/hdp/2.4.0.0-169/hadoop/lib/native -Djava.library.path=/usr/hdp/2.4.0.0-169/hadoop/lib"
-#export HADOOP_COMMON_LIB_NATIVE_DIR=/usr/hdp/2.4.0.0-169/hadoop/lib/native
 
-./kafka2hdfs -c conf/kafka2hdfs_config_example.conf
+#./kafka2hdfs -c conf/kafka2hdfs_config_example.conf
