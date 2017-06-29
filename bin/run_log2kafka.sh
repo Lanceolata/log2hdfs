ed87060fe7afc9e958ee54535dbd44b363424bf6
lanceolata <lanceolatayuan@gmail.com>
2017-06-29 11:50:04 +0800

    update

--- bin/run_log2kafka.sh
+++ bin/run_log2kafka.sh
@@ -1,31 +1,24 @@
 #!/usr/bin/env bash
 
+ulimit -c unlimited
 export LD_LIBRARY_PATH="../log2hdfs/thirdparty/installed/lib:$LD_LIBRARY_PATH"
 
 type=$1
 
-if [ "$type" == "adp" ]
+array=("adp" "adp-unbid" "dsp" "stats" "stats-cm" "stats-cmo" "imp" "imp-v6" "sandbox")
+
+for i in ${array[@]}
+do
+    if [ "$type" == "$i" ]
+    then
+        valid=1
+    fi
+done
+
+if [ ! -n "$valid" ]
 then
-    ../log2hdfs/bin/log2kafka -c adp.conf -l adp-log.conf > adp-stderr.log 2>&1 &
-elif [ "$type" == "adp-unbid" ]
-then
-    ../log2hdfs/bin/log2kafka -c adp-unbid.conf -l adp-unbid-log.conf > adp-unbid-stderr.log 2>&1 &
-elif [ "$type" == "dsp" ]
-then
-    ../log2hdfs/bin/log2kafka -c dsp.conf -l dsp-log.conf > dsp-stderr.log 2>&1 &
-elif [ "type" == "stats" ]
-then
-    ../log2hdfs/bin/log2kafka -c stats.conf -l stats-log.conf > stats-stderr.log 2>&1 &
-elif [ "type" == "stats-cm" ]
-then
-    ../log2hdfs/bin/log2kafka -c stats-cm.conf -l stats-cm-log.conf > stats-cm-stderr.log 2>&1 &
-elif [ "type" == "stats-cmo" ]
-then
-    ../log2hdfs/bin/log2kafka -c stats-cmo.conf -l stats-cmo-log.conf > stats-cmo-stderr.log 2>&1 &
-elif [ "$type" == "sandbox" ]
-then
-    ../log2hdfs/bin/log2kafka -c sandbox.conf -l sandbox.conf > sandbox-stderr.log 2>&1 &
-else
     echo "unknown type:$type"
     exit 1
 fi
+
+echo "../log2hdfs/bin/log2kafka -c $type.conf -l $type-log.conf > $type-stderr.log 2>&1 &"
